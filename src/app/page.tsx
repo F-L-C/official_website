8a30628d152381ab8c1aad8d3fa8c9bd0e0aa581
Soham Parate <sohamparate1905@gmail.com>
2023-08-06 23:58:59 +0530

    improved project structure and designed a basic skeleton for page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import Image from "next/image";
+import Main from './components/Main';
+import Navbar from './components/Navbar';
 
-export default function Home() {
-  return <>
-    <h1 className="underline text-flc-blue">Hello World</h1>
-  </>;
-}
+const HomePage = () => {
+  return (
+    <div className='relative'>
+      <div
+        className="fixed top-0 left-0 w-full h-full bg-bg bg-fixed bg-cover"
+      ></div>
+      <div className="relative z-10">
+        <Navbar />
+        <Main />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;

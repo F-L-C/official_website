3b7a4a2580ec190d9d0c5a29acc1f3f15c0d8543
Soham Parate <sohamparate1905@gmail.com>
2023-08-06 23:32:11 +0530

    Fixed a problem where tailwindcss default colors weren't available to use

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,18 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      'flc-green': '#0073BB',
-      'flc-darkgreen': '#1A3702',
-      'flc-blue': '#0073BB',
-      'flc-white': '#F8FFF8'
-    },
     extend: {
+      colors: {
+        'flc-green': '#A8D39D',
+        'flc-darkgreen': '#1A3702',
+        'flc-blue': '#0073BB',
+        'flc-white': '#F8FFF8'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'bg': "url('/assets/bg.jpg')",
       },
     },
   },

1678b14506c15fd594c7eee6c758a761cb906d28
Parth <parthwani22@gmail.com>
2023-08-06 20:16:30 +0530

    Add sections and create styles and assets folders.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,26 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <>
-    <h1 className="underline text-flc-blue">Hello World</h1>
-  </>;
+  return (
+    <>
+      <section className="Home">
+        <h1>This is Home Section</h1>
+      </section>
+      <section className="About">
+        <h1>This is About Section</h1>
+      </section>
+      <section className="Events">
+        <h1>This is Events Section</h1>
+      </section>
+      <section className="Team">
+        <h1>This is Team Section</h1>
+      </section>
+      <section className="Blog">
+        <h1>This is Blog Section</h1>
+      </section>
+      <section className="Contact">
+        <h1>This is Contact Section</h1>
+      </section>
+    </>
+  );
 }

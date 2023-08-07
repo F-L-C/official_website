734809d6481270f0a5baab0bf919215c91748d9c
Parth <parthwani22@gmail.com>
2023-08-07 08:44:00 +0530

    Changed section to div. Added folders.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,24 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <section className="Home">
+      <div className="Home">
         <h1>This is Home Section</h1>
-      </section>
-      <section className="About">
+      </div>
+      <div className="About">
         <h1>This is About Section</h1>
-      </section>
-      <section className="Events">
+      </div>
+      <div className="Events">
         <h1>This is Events Section</h1>
-      </section>
-      <section className="Team">
+      </div>
+      <div className="Team">
         <h1>This is Team Section</h1>
-      </section>
-      <section className="Blog">
+      </div>
+      <div className="Blog">
         <h1>This is Blog Section</h1>
-      </section>
-      <section className="Contact">
+      </div>
+      <div className="Contact">
         <h1>This is Contact Section</h1>
-      </section>
+      </div>
     </>
   );
 }

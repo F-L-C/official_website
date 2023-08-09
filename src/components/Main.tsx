576ebdb30e5951a409fd282102906ed0797c087e
Soham Parate <sohamparate1905@gmail.com>
2023-08-09 17:01:49 +0530

    linting errors

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,7 +6,7 @@ const Main = () => {
 			<div className='flex justify-center items-center'>
 				<div className='bg-white h-full w-3/4 text-black'>
 					<h1 className=''>
-                        hi
+						hi
 					</h1>
 					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia cum, vero ipsa temporibus ullam et. Vero consequuntur voluptatum doloribus natus?</p>
 				</div>
